0fa03d70484105ab35c958ef0c62ac11da2fdf40
andrehoffmannbr <andrehoffmannmk@gmail.com>
2025-06-17 16:25:46 -0300

    fix: Corrige erros de JavaScript e URLs das serverless functions

--- api/criar-pagamento-completo.js
+++ api/criar-pagamento-completo.js
@@ -1,6 +1,6 @@
 const mercadopago = require('mercadopago');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // Configurar CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
